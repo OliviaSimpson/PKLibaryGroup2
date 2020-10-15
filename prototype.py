80c1bf0da1c80250524f9cc2d3593cc3246455c3
Matthew Holland <matthew.holland@spc.ox.ac.uk>
2020-10-15 15:12:38 +0100

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -4,15 +4,16 @@ import scipy.integrate
 
 def dose(t, X):
     return X
-
+# Dose function - X is dose in nanograms, t indicates that the dosing protocol could involve more than one dose at various timepoints, or a continuous application
+# will need to modify this function to take this into account 
 def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
-    q_c, q_p1 = y
-    transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-    dqc_dt = dose(t, X) - q_c / V_c * CL - transition
-    dqp1_dt = transition
+    q_c, q_p1 = y                                           #sets the initial drug quantity in the central and peripheral compartments
+    transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)           #determines the transition rate (in ng/h)
+    dqc_dt = dose(t, X) - q_c / V_c * CL - transition       #Rate of change of qc (in ng/h)
+    dqp1_dt = transition                                    #Rate of change of qp1 (in ng/h) aka the quantity of drug in peripheral compartment
     return [dqc_dt, dqp1_dt]
 
-model1_args = {
+model1_args = {                                             #These are both libraries of parameters for the model, changing the transition rate between model 1 and 2
     'name': 'model1',
     'Q_p1': 1.0,
     'V_c': 1.0,
@@ -30,8 +31,8 @@ model2_args = {
     'X': 1.0,
 }
 
-t_eval = np.linspace(0, 1, 1000)
-y0 = np.array([0.0, 0.0])
+t_eval = np.linspace(0, 1, 1000)                        #generating the time points to evaluate the model at
+y0 = np.array([0.0, 0.0])                               
 
 fig = plt.figure()
 for model in [model1_args, model2_args]:
