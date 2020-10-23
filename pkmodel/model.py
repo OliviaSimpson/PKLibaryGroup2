32fab3a4ac2f85e37e98cd0cde2e3722576a5d32
james bayne <jamesbayne@JamesMacbook.local>
2020-10-23 12:08:07 +0100

    push changes to model (includes rough code for the solution)

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -4,24 +4,36 @@ class Model:
     Parameters
     ----------
 
-    :param params: A 2D list or tuple containing the parameters for the compartments, with the first list/tuple being
-    the central compartment and the rest being the peripherals
-    :type params: list or tuple
+    :param params: A 2D list containing the parameters for the compartments, with the first list being
+    the central compartment and the rest being the peripherals. Must be specified in the order [rate, volume].
+    :type params: list
+    :param k_a": If modelling subcutaneous dosing this is the absorption rate
+    :type k_a: float, optional
     """
 
-    def __init__(self, params):
+    def __init__(self, params, k_a=0):
         # check that input is suitable
-        if len(params) < 2:
-            raise ValueError('not enough compartments specified')
+        if not any(isinstance(i, list) for i in params):
+            raise TypeError('input must be supplied as a 2D list')
         for p in params:
             if len(p) != 2:
                 raise ValueError('incorrect number of parameters provided')
 
-        # make the model
+        # make central compartment
         self.central = Compartment(params[0], type='central')  # make central compartment
+        self.cl = self.central.rate
+        self.v_c = self.central.volume
+
+        # make peripherals
         self.peripherals = []
+        self.Q_ps = []  # store the Q_p values for each peripheral
+        self.v_ps = []  # store the v_p values for each peripheral
         for p in params[1:]:  # make peripheral compartments and store in a list
-            self.peripherals.append(Compartment(p, type='peripheral'))
+            peripheral = Compartment(p, type='peripheral')
+            self.peripherals.append(peripheral)
+            self.Q_ps.append(peripheral.rate)
+            self.v_ps.append(peripheral.volume)
+        self.k_a = k_a  # absorption rate for subcutaneous dosing (defaults to 0)
 
 
 class Compartment:
@@ -44,5 +56,24 @@ class Compartment:
         self.type = str(type)
 
 
-model = Model([[0, 1], [2, 4], [3, 5]])
-print(model.central)
+model = Model([[0, 1]])
+print(model.v_c)
+
+v_ps = [1,2,3]
+q_ps = [4,5,6]
+
+def make_equation(v_ps, q_ps):
+    peris = []
+    for vi, qi in v_ps, q_ps:
+        peris.append(qi * ())
+
+def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
+    transitions = [] # store the transitions
+    for vi, qi, i in v_ps, q_ps, range(len(v_ps)):
+        transitions.append(qi * (y[0] / V_c - y[i] / vi))
+    dqc_dt = dose(t, X) - q_c / V_c * CL
+    for t in transitions:
+        dqc_dt -= t
+    return transitions.insert(0, dqc_dt)
+
+
