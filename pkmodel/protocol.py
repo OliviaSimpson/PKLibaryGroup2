1482812111e9a02a8d49386df06eab12a6ee3c7b
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2020-10-23 13:22:46 +0100

    Reworked protocol class

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -1,6 +1,7 @@
 #
 # Protocol class
 #
+import math
 
 class Protocol:
     """A class to describe the dosing protocol and method of administration for the pharmacokinetic model.
@@ -10,26 +11,37 @@ class Protocol:
     :type d_g: float
     :param dur: the total duration of continuous infusion (hr)
     :type dur: float
-    :param n_dose: the number of doses to be administered in a discrete bolus protocol
-    :type n_dose: int
-    :param method: the method of administration (intravenous or subcutaneous)
-    :type method: string
+    :param dose_times: the timing of administration of each bolus dose (hr)
+    :type n_dose: tuple
     """
     
-    def __init__(self, d_g, dur, plan, method):
+    def __init__(self, d_g, dur, dose_times):
         self.d_g = d_g # total quantity of drug
         self.dur = dur # continuous: duration of infusion
-        self.n_dose = n_dose # discrete: number of repeated doses
-        self.method = method # method of administration
+        self.dose_times = dose_times # discrete: number of repeated doses - tuple
+        # assumes that the dose of drug with each discrete bolus is equal
 
     def continuous(self):
-        return self.d_g / self.dur
+        if self.dur == 0:
+            return 0
+        else:
+            return self.d_g / self.dur
 
     def discrete(self):
-        a = self.n_dose / (self.d_g * math.sqrt(math.pi))
-        return (1 / (a * math.sqrt(math.pi))) * math.exp(-(t / a) ** 2)
+        if self.dose_times == ():
+            return 0
 
+        else:
+            n_dose = len(dose_times)
+            gaussians = []
+            a = self.n_dose / (self.d_g * math.sqrt(math.pi))
+            for time in dose_times:
+                gaussian = (1 / (a * math.sqrt(math.pi))) * math.exp(-(time / a) ** 2)
+                gaussians.append(gaussian)
+            return gaussians
 
+    def dose(self, t):
+        return self.continuous() + self.discrete()
 
 
 
