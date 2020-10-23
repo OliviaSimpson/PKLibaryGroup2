cf3964a3228667b3cf1c3c6a47ed55504a1202d3
Stephanie Wills <stephanie.wills@balliol.ox.ac.uk>
2020-10-23 17:50:32 +0100

    New draft for protocol

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -2,6 +2,7 @@
 # Protocol class
 #
 import math
+import numpy as np
 
 class Protocol:
     """A class to describe the dosing protocol and method of administration for the pharmacokinetic model.
@@ -9,24 +10,23 @@ class Protocol:
     Parameters
     :param d_g: the total quantity of drug to be administered over the entire protocol (ng)
     :type d_g: float
-    :param dur: the total duration of continuous infusion (hr)
-    :type dur: float
-    :param dose_times: the timing of administration of each bolus dose (hr)
-    :type dose_times: tuple
+    :param plan: the duration of infusion (if continuous); the timings of each dose (if discrete)
+    :type plan: list or tuple (if continuous); int or float (if discrete)
     """
+    
     def __init__(self, d_g, plan):
         self.d_g = d_g
         self.plan = plan
 
     def dose(self):
-        if isinstance(self.plan,list) or isinstance(self.plan, tuple): # discrete administration
+        if isinstance(self.plan, list) or isinstance(self.plan, tuple): # discrete administration
             n_dose = len(self.plan)
-            gaussians = []
-            a = n_dose / (self.d_g * math.sqrt(math.pi))
-            for time in self.plan:
-                gaussian = (1 / (a * math.sqrt(math.pi))) * math.exp(-(time / a) ** 2)
-                gaussians.append(gaussian)
-            return gaussians
+
+            sig = 1 / ((self.d_g / n_dose) * math.sqrt(2 * math.pi))
+
+            gaussian = sum((1 / sig * math.sqrt(2 * math.pi)) * math.exp(-0.5 * ((t - dose_time) ** 2 / (sig ** 2))for dose_time in self.plan)
+            
+            return gaussian
 
         elif isinstance(self.plan, int) or isinstance(self.plan, float): # continuous administration
             return self.d_g / self.plan
@@ -34,6 +34,8 @@ class Protocol:
         else:
             raise TypeError('incorrect input format')
 
+protocol = Protocol(5, [1,2,3])
 
-protocol = Protocol(3,[3,4])
-print(protocol.dose())
+
+        
+
